bcd66ed7c3500c5b3b1d1cd9d3a80687114db126
James Blackledge <james.blackledge@ada.ac.uk>
2017-09-27 22:09:48 +0100

    Added Tester Code

--- arduino-code/arduino-code.ino
+++ arduino-code/arduino-code.ino
@@ -1,42 +1,32 @@
-#include <HashMap.h>
-
-int led_flash[] = {};
-int cor_button[] = {};
-
-const int led_one = 1;
-const int led_two = 2;
-const int led_three = 3;
-const int led_four = 4;
-
-const int button_one = 5;
-const int button_two = 6;
-const int button_three = 7;
-const int button_four = 8;
-
-int button_state = 0;
-
-int user_difficulty = 0;
-int flash_rate = 0;
-
-int correct = 0;
-int wrong = 0;
-
-const byte HASH_SIZE = 4;
-HashType<int, int> hashRawArray[HASH_SIZE];
-HashMap<int, int> led_button = HashMap<int, int>(\hashRawArray, HASH_SIZE);
-
 void setup() {
 
-  led_button[0](led_one, button_one);
-  led_button[1](led_two, button_two);
-  led_button[2](led_three, button_three);
-  led_button[3](led_four, button_four);
-
-  Serial.begin(9600);
-  Serial.println(led_button.getValueOf(led_two));
-  
-  led_button.debug();
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);  
 }
 
+void loop() {
 
+  digitalWrite(1, HIGH);
+  delay(1000);
+  digitalWrite(1, LOW);
+  delay(1000);
+  
+  digitalWrite(2, HIGH);
+  delay(1000);
+  digitalWrite(2, LOW);
+  delay(1000);
+  
+  digitalWrite(3, HIGH);
+  delay(1000);
+  digitalWrite(3, LOW);
+  delay(1000);
+
+  digitalWrite(4, HIGH);
+  delay(1000);
+  digitalWrite(4, LOW);
+  delay(1000);
+
+}
 
