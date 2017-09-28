19ce1b6e69b0a196bbf1833abe1c5d3c7f4d1cc8
James Blackledge <james.blackledge@ada.ac.uk>
2017-09-28 11:27:17 +0100

    Finished Data Repository

--- arduino-code/arduino-code.ino
+++ arduino-code/arduino-code.ino
@@ -1,32 +1,54 @@
+int led_list[4];
+
+const int led_one = 1;
+const int led_two = 2;
+const int led_three = 3;
+const int led_four = 4;
+
+const int button_one = 1;
+const int button_two = 2;
+const int button_three = 3;
+const int button_four = 4;
+
+typedef struct {
+
+  int led;
+  int button;
+  
+} led_button_map;
+
+const led_button_map led_button[] {
+
+  {led_one, button_one},
+  {led_two, button_two},
+  {led_three, button_three},
+  {led_four, button_four}
+  
+};
+
 void setup() {
 
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);  
+  for (int i = 0; i <= 4; i++) {
+
+    led_list[i] = led_button[i].led;
+    
+  }
+
+  for (int j = 0; j <= 4; j++) {
+
+    pinMode(led_list[j], OUTPUT);
+    
+  }
+  
 }
 
 void loop() {
 
-  digitalWrite(1, HIGH);
-  delay(1000);
-  digitalWrite(1, LOW);
-  delay(1000);
-  
-  digitalWrite(2, HIGH);
-  delay(1000);
-  digitalWrite(2, LOW);
-  delay(1000);
-  
-  digitalWrite(3, HIGH);
-  delay(1000);
-  digitalWrite(3, LOW);
-  delay(1000);
+  for (int k = 0; k <= 4; k++) {
 
-  digitalWrite(4, HIGH);
-  delay(1000);
-  digitalWrite(4, LOW);
-  delay(1000);
-
+    digitalWrite(led_list[k], HIGH);
+    
+  }
+  
 }
 
