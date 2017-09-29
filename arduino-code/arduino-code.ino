15491822628d816563fbd181359107c1017a1f8b
James Blackledge <james.blackledge@ada.ac.uk>
2017-09-29 14:41:48 +0100

    C++; LEDs Flash Randomly

--- arduino-code/arduino-code.ino
+++ arduino-code/arduino-code.ino
@@ -11,44 +11,61 @@ const int button_three = 3;
 const int button_four = 4;
 
 typedef struct {
-
   int led;
   int button;
-  
 } led_button_map;
 
-const led_button_map led_button[] {
-
+const led_button_map led_button[]{
   {led_one, button_one},
   {led_two, button_two},
   {led_three, button_three},
   {led_four, button_four}
-  
 };
 
-void setup() {
+int randomizeList() {
 
-  for (int i = 0; i <= 4; i++) {
+  unsigned char chosen[sizeof(led_list)];
+  unsigned char i, j;
 
-    led_list[i] = led_button[i].led;
-    
+  for (i = 0; i < sizeof(led_list); i++){
+    chosen[i] = 0;
   }
 
-  for (int j = 0; j <= 4; j++) {
+  randomSeed(analogRead(A0));
 
+  for (i = 0; i < sizeof(led_list); i++){
+    int r = random(sizeof(led_list) - i);
+    for (j = 0; j <= r; j++){
+      r += chosen[j];
+    }
+    chosen[r] = 1;
+    led_list[i] = r;
+  }
+  
+}
+
+void setup() {
+  
+  for (int i = 0; i <= sizeof(led_list); i++){
+    led_list[i] = led_button[i].led;
+  }
+
+  randomizeList();
+
+  for (int j = 0; j <= sizeof(led_list); j++){
     pinMode(led_list[j], OUTPUT);
-    
   }
   
 }
 
 void loop() {
-
-  for (int k = 0; k <= 4; k++) {
-
-    digitalWrite(led_list[k], HIGH);
-    
-  }
   
+  for (int i = 0; i <= sizeof(led_list); i++){
+    digitalWrite(led_list[i], HIGH);
+    delay(500);
+    digitalWrite(led_list[i], LOW);
+  }
+
+  delay(1000000000);
 }
 
